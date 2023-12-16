4592930c60f90a66c6902a2f77a792c6bf63c0b0
allyndawn <allyn.snook@gmail.com>
2023-12-15 16:52:20 -0800

    Link library; draw a little

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
-INC=-I../rpi-rgb-led-matrix/include
+RGB_LIB_DISTRIBUTION=../rpi-rgb-led-matrix
+RGB_INCDIR=$(RGB_LIB_DISTRIBUTION)/include
+RGB_LIBDIR=$(RGB_LIB_DISTRIBUTION)/lib
+RGB_LIBRARY_NAME=rgbmatrix
+RGB_LIBRARY=$(RGB_LIBDIR)/lib$(RGB_LIBRARY_NAME).a
+LDFLAGS+=-L$(RGB_LIBDIR) -l$(RGB_LIBRARY_NAME) -lrt -lm -lpthread
 
 all: eyes
 
-eyes: eyes.o
-	g++ -o eyes eyes.o
+eyes: eyes.o $(RGB_LIBRARY)
+	g++ -o eyes eyes.o $(LDFLAGS)
 
 eyes.o: eyes.cpp
-	g++ $(INC) -c eyes.cpp
+	g++ -I$(RGB_INCDIR) -c eyes.cpp
--- eyes.cpp
+++ eyes.cpp
@@ -1,11 +1,55 @@
 #include <iostream>
 
+#include <signal.h>
+#include <unistd.h>
+
 #include "led-matrix.h"
 
 using namespace std;
 
+using rgb_matrix::RGBMatrix;
+using rgb_matrix::Canvas;
+
+volatile bool interrupt_received = false;
+
+static void InterruptHandler(int signo) {
+	interrupt_received = true;
+}
+
+static void DrawOnCanvas(Canvas *canvas) {
+	for (int i = 0; i<32; i++) {
+		if (interrupt_received) {
+			return;
+		}
+
+		canvas->SetPixel(i, i, 255, 0, 0);
+
+		usleep(10000);
+	}
+}
+
 int main(int argc, char **argv) {
 	cout << "Hello World" << endl;
+
+	RGBMatrix::Options defaults;
+	defaults.hardware_mapping = "regular";
+	defaults.rows = 32;
+	defaults.chain_length = 1;
+	defaults.parallel = 1;
+	defaults.show_refresh_rate = true;
+	Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
+	if (canvas == NULL) {
+		cout << "Unable to create canvas" << endl;
+		return 1;
+	}
+	signal(SIGTERM, InterruptHandler);
+	signal(SIGINT, InterruptHandler);
+
+	DrawOnCanvas(canvas);
+
+	canvas->Clear();
+	delete canvas;
+
 	return 0;
 }
 
